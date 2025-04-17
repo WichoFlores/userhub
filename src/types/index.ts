0b83e06b2385bca0b65ef2109d295ecdb35b93ed
Luis Flores <aetherwicho@hotmail.com>
2025-04-17 02:09:55 -0600

    feat: add mock user data

---
+++ src/types/index.ts
@@ -0,0 +1,11 @@
+export interface User {
+  id: string;
+  name: string;
+  avatarSeed: string;
+  email: string;
+  location: string;
+  bio: string;
+  jobTitle: string;
+  company: string;
+  friendIds: string[];
+}

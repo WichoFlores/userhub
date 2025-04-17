90ab6b9179077ad4cde3b6eaa43e5943661d9044
Luis Flores <aetherwicho@hotmail.com>
2025-04-17 03:35:45 -0600

    fix: add user list revalidation to update while using cache

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { UserGrid } from "@/components/UserGrid";
 async function fetchUsers() {
   const res = await fetch("http://localhost:3000/api/users", {
     cache: "force-cache",
+    next: { revalidate: 60 },
   });
   return res.json();
 }
